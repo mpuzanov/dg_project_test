fe2b8dcd7328bf884b76e5097cab05993ac1ec46
Mikhail Puzanov <mpuzanov@mail.ru>
2023-10-06 18:36:04 +0400

    табличные типы

---
+++ db_kvart/dbo/table-type/myTypeTableOcc.sql
@@ -0,0 +1,25 @@
+create type myTypeTableOcc as table
+(
+    occ            int not null,
+    service_id     varchar(10) collate SQL_Latin1_General_CP1251_CI_AS,
+    fin_id         smallint,
+    tip_id         smallint,
+    flat_id        int,
+    build_id       int,
+    summa1         decimal(9, 2),
+    summa2         decimal(9, 2),
+    summa3         decimal(9, 2),
+    PROPTYPE_ID    varchar(10) collate SQL_Latin1_General_CP1251_CI_AS,
+    ROOMTYPE_ID    varchar(10) collate SQL_Latin1_General_CP1251_CI_AS,
+    TOTAL_SQ       decimal(6, 2),
+    address        varchar(60) collate SQL_Latin1_General_CP1251_CI_AS,
+    sup_id         int,
+    dog_int        int,
+    occ_sup        int,
+    kol_people     smallint,
+    kol_people_reg smallint,
+    primary key (
+                 occ)
+)
+go
+
